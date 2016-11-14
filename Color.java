08140c6f3a4750fc228d0c43bccfe668f00ef591
Teodor Hurtigh Isaacs <Teodor.hurtigh@gmail.com>
2016-11-14 16:09:12 +0100

    Instructions and instruction classes

--- Color.java
+++ Color.java
@@ -3,6 +3,6 @@ package S2medHybris;
 /**
  * Created by Teodor Isaacs on 16-11-14.
  */
-public class Left extends Instruction {
-    private int degree;
+public class Color extends Instruction{
+    private String color;
 }
