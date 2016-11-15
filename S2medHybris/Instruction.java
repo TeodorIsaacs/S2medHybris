e7200e48e6efd1fd1e70a31d2a29ca151230ac15
Teodor Hurtigh Isaacs <Teodor.hurtigh@gmail.com>
2016-11-15 11:55:27 +0100

    Nyaste versionen

--- S2medHybris/Instruction.java
+++
@@ -1,8 +0,0 @@
-package S2medHybris;
-
-/**
- * Created by Teodor Isaacs on 16-11-14.
- */
-public abstract class Instruction {
-    public abstract String printableInfo();
-}
--- S2medHybris/Main.java
+++ S2medHybris/Main.java
@@ -7,17 +7,5 @@ import java.util.ArrayList;
  */
 public class Main {
     public static void main(String[] args) {
-        Instruction inst = whichInstruction("#101010");
-        System.out.println(inst.printableInfo());
-    }
-
-    private static Instruction whichInstruction(String line){
-        Color mbyColor = Color.isThisInstruction(line);
-        if (mbyColor != null)
-            return mbyColor;
-        Back mbyBack = Back.isThisInstruction(line);
-        if (mbyBack != null)
-            return mbyBack;
-        return null;
     }
 }
